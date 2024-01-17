51b36b8e11e8e6de1a14d262868e9bee8152b1df
Marco Schoch <marco.schoch@hftm.ch>
2024-01-17 22:18:13 +0100

    #IMPROVEMENT add list of heros

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.less'
+  styleUrl: './heroes.component.less',
 })
 export class HeroesComponent {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
 
 }
