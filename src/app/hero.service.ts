21d769fa98cc01c06880e5c8e98f50d8b5a8bd3d
Marco Schoch <marco.schoch@hftm.ch>
2024-01-31 22:22:59 +0100

    #IMPROVEMENT add post method for changeing heronames

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,21 +3,26 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
   
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService, private httpClient: HttpClient) { }
 
   getHeroes(): Observable<Hero[]> {
     this.messageService.addMessage('HeroService fetched all the heroes')
-    return of(HEROES);
+    return this.httpClient.get<Hero[]>('http://127.0.0.1:5000/heroes');
   }
 
   getHero(id: number): Observable<Hero> {
     this.messageService.addMessage(`We grapped the Hero with id ${id}`)
-    return of(HEROES.find(hero => hero.id == id)!)
+    return this.httpClient.get<Hero>(`http://127.0.0.1:5000/detail/${id}`);
+  }
+
+  updateHero(hero: Hero): Observable<Hero> {
+    return this.httpClient.post<Hero>('http://127.0.0.1:5000/update', hero)
   }
 }
